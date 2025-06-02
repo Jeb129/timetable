74e088bf300da99ed9df14f4522e331e3f3e0bed
Jeb129 <elyer129@gmail.com>
2025-06-03 00:14:02 +0300

    Роуты update_object  и delete_object

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('create/<str:model_name>/', create_object),
-    path('get/<str:object_name>/<int:object_id>/',get_object)
+    path('get/<str:object_name>/<int:object_id>/',get_object),
+    path('update/<str:object_name>/<int:object_id>/',update_object),
+    path('delete/<str:object_name>/<int:object_id>/',delete_object),
 ]
